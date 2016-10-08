da4d489c5a40280c0ec2d0e5b9de0daf52a58d04
Chen Shou <shou3301@outlook.com>
2016-10-08 14:51:30 -0700

    L122

---
+++ Warrirors/Leet/122.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) return 0;
+        int res = 0;
+        for (int i = 1; i < prices.length; i++) {
+            res += Math.max(0, prices[i] - prices[i - 1]);
+        }
+
+        return res;
+    }
+}
