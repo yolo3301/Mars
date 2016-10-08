d55bf0302900758ed8be668b343a6684f9c477b3
Chen Shou <shou3301@outlook.com>
2016-10-08 14:33:11 -0700

    L382

---
+++ Warrirors/Leet/382.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+
+    private ListNode head;
+    private Random random;
+
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    public Solution(ListNode head) {
+        this.head = head;
+        this.random = new Random();
+    }
+
+    /** Returns a random node's value. */
+    public int getRandom() {
+        ListNode curr = head;
+        ListNode res = null;
+
+        for (int i = 1; curr != null; i++) {
+            if (random.nextInt(i) == 0) {
+                res = curr;
+            }
+
+            curr = curr.next;
+        }
+
+        return res.val;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
