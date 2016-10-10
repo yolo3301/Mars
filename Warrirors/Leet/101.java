9268f6e18fae04311b8b83ae4bf7b62bbc6ffede
Chen Shou <shou3301@outlook.com>
2016-10-09 23:01:31 -0700

    L101

---
+++ Warrirors/Leet/101.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return compare(root.left, root.right);
+    }
+
+    private boolean compare(TreeNode n1, TreeNode n2) {
+        if (n1 == null && n2 == null) return true;
+        if (n1 == null || n2 == null) return false;
+        return n1.val == n2.val && compare(n1.left, n2.right) && compare(n1.right, n2.left);
+    }
+}
