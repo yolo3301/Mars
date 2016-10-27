fda5d8dab91e15e186ce9154e66dea8ae7f32bbc
Chen Shou <shou3301@outlook.com>
2016-10-26 23:36:33 -0700

    L11

---
+++ Warrirors/Leet/11.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int maxArea(int[] height) {
+        int i = 0, j = height.length - 1;
+        int max = 0;
+        while (i < j) {
+            max = Math.max(max, (j - i) * Math.min(height[i], height[j]));
+            if (height[i] <= height[j]) i++;
+            else j--;
+        }
+        return max;
+    }
+}
