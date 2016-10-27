d459d98dc9e46c85a2c8baa84e034e20808e66e9
Chenshi <ducs1988@gmail.com>
2016-10-26 22:17:09 -0500

    LT287_cdu

---
+++ ChuiNiuBi/287.java
@@ -0,0 +1,33 @@
+/*
+Find the Duplicate Number
+
+Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+
+Note:
+You must not modify the array (assume the array is read only).
+You must use only constant, O(1) extra space.
+Your runtime complexity should be less than O(n2).
+There is only one duplicate number in the array, but it could be repeated more than once.
+*/
+
+public class Solution {
+    public int findDuplicate(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return -1;
+            
+        int l = 1, r = nums.length - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            int count = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] <= mid)
+                    count++;
+            }
+            if (count > mid)
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return l;
+    }
+}
