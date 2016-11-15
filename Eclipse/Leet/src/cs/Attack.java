d19a18ce6780d47a3f963a2db4e81697ba5ba62d
Chen Shou <shou3301@outlook.com>
2016-11-14 23:18:15 -0800

    L95

--- Eclipse/Leet/src/cs/Attack.java
+++ Eclipse/Leet/src/cs/Attack.java
@@ -1,12 +1,17 @@
 package cs;
 
-import java.util.Arrays;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Attack {
 
 	public static void main(String[] args) {
-		Leet400 l = new Leet400();
-		System.out.println(l.findNthDigit(190));
+		//Leet400 l = new Leet400();
+		//System.out.println(l.findNthDigit(190));
+		ArrayList<MyType>[][] = (ArrayList<MyType>[][]) Array.newInstance(ArrayList.class, 5, 5);
 	}
 
+	public class MyType {
+		public int x;
+	}
 }
