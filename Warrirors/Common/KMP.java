76725b4fb6584015e2a20d04801c3a38c162087f
Chen Shou <shou3301@outlook.com>
2017-01-02 20:59:54 -0800

    common/kmp

---
+++ Warrirors/Common/KMP.java
@@ -0,0 +1,42 @@
+// http://www.matrix67.com/blog/archives/115
+// https://tekmarathon.com/2013/05/14/algorithm-to-find-substring-in-a-string-kmp-algorithm/
+public class KMP {
+    public int[] preProcess(char[] pattern) {
+        int len = pattern.length;
+        int[] b = new int[len+1];
+        b[0] = -1;
+        int i = 0, j = 1;
+
+        while (i < len) {
+            while (j >= 0 && pattern[i] != pattern[j]) {
+                j = b[j];
+            }
+            i++;
+            j++;
+            b[i] = j;
+        }
+
+        return b;
+    }
+
+    public List<Integer> match(char[] text, char[] pattern) {
+        List<Integer> res = new ArrayList<>();
+        int[] b = preProcess(pattern);
+        int i = 0, j = 0;
+
+        while (i < text.length) {
+            while (j >= 0 && pattern[j] != text[i]) {
+                j = b[j];
+            }
+
+            i++;
+            j++;
+            if (j == pattern.length) {
+                res.add(j - pattern.length);
+                j = b[j];
+            }
+        }
+
+        return res;
+    }
+}
