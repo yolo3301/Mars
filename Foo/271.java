1b2c1f30aa706ca34f2d73609d363141ef4b06b8
Chen Shou <shou3301@outlook.com>
2017-05-12 00:05:39 -0700

    foo-271

---
+++ Foo/271.java
@@ -0,0 +1,26 @@
+public class Codec {
+
+    // Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for (String s : strs) {
+            sb.append(s.replace("#", "##")).append(" # ");
+        }
+        return sb.toString();
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode(String s) {
+        List<String> res = new ArrayList<>();
+        String[] strs = s.split(" # ", -1);
+        for (int i = 0; i < strs.length-1; i++) {
+            res.add(strs[i].replace("##", "#"));
+        }
+
+        return res;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(strs));
