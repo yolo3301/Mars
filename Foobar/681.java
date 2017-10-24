27b1b0e50099fee0db2c284c9902ec9ed4980596
chshou <shou3301@outlook.com>
2017-10-23 23:19:03 -0700

    foo2-681

--- Foobar/681.java
+++ Foobar/681.java
@@ -1,55 +1,34 @@
 class Solution {
-    public String nextClosestTime(String time) {
-        String[] t = time.split(":");
-        int h = Integer.parseInt(t[0]);
-        int m = Integer.parseInt(t[1]);
-        Set<Integer> cand = new HashSet<>();
-        cand.add(h % 10);
-        cand.add(h / 10);
-        cand.add(m % 10);
-        cand.add(m / 10);
+    public int kEmptySlots(int[] flowers, int k) {
+        // reverse index
+        int[] days = new int[flowers.length];
+        for (int i = 0; i < flowers.length; i++) {
+            days[flowers[i]-1] = i+1;
+        }
 
-        if (cand.size() == 1) return time;
+        // keep a k-ish window
+        int left = 0, right = k+1, ret = Integer.MAX_VALUE;
+        for (int i = 0; right < days.length; i++) {
+            /*
+            1. What does days[i] < days[left] or days[right] mean?
+                it means some floor between left & right bloomed early than them
+            2. Why it's <= days[right]?
+                to handle == case
+            3. Why it's ok to set left to i when condition meets?
+                because previous ones are for sure greater than the current one
+                which means the current floor will not work for previous ones
+            */
+            if (days[i] < days[left] || days[i] <= days[right]) {
+                if (days[i] == days[right]) {
+                    // Find a candidate
+                    ret = Math.min(ret, Math.max(days[left], days[right]));
+                }
 
-        List<Integer> list = new ArrayList<>(cand);
-        Collections.sort(list);
-
-        if (m % 10 < 9) {
-            int r = nextGreater(list, m % 10);
-            if (r != -1) {
-                return new StringBuilder().append(t[0]).append(":").append(m/10).append(r).toString();
+                left = i;
+                right = i+k+1;
             }
         }
 
-        if (m / 10 < 5) {
-            int r = nextGreater(list, m / 10);
-            if (r != -1 && r <= 5) {
-                return new StringBuilder().append(t[0]).append(":").append(r).append(list.get(0)).toString();
-            }
-        }
-
-        if (h < 23 && h % 10 < 9) {
-            int r = nextGreater(list, h % 10);
-            if (r != -1) {
-                return new StringBuilder().append(h/10).append(r).append(":").append(list.get(0)).append(list.get(0)).toString();
-            }
-        }
-
-        if (h % 10 == 9) {
-            int r = nextGreater(list, h / 10);
-            if (r != -1) {
-                return new StringBuilder().append(r).append(list.get(0)).append(":").append(list.get(0)).append(list.get(0)).toString();
-            }
-        }
-
-        return new StringBuilder().append(list.get(0)).append(list.get(0)).append(":").append(list.get(0)).append(list.get(0)).toString();
-    }
-
-    private int nextGreater(List<Integer> list, int curr) {
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i) > curr) return list.get(i);
-        }
-
-        return -1;
+        return ret== Integer.MAX_VALUE ? -1 : ret;
     }
 }
