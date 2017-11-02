2161cd78ec61be21ad00499e638992fa222b237e
chshou <shou3301@outlook.com>
2017-11-01 23:31:16 -0700

    foo2-305

--- Foobar/305.java
+++ Foobar/305.java
@@ -1,42 +1,44 @@
 class Solution {
     public List<Integer> numIslands2(int m, int n, int[][] positions) {
-        int[][] dirs = new int[][]{{0,1}, {0,-1}, {1,0}, {-1,0}};
+        List<Integer> ret = new ArrayList<>();
+        int cnt = 0;
         int[] root = new int[m*n];
+
         Arrays.fill(root, -1);
 
-        List<Integer> res = new ArrayList<>();
-        int cnt = 0;
+        int[][] dirs = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
+
         for (int[] pos : positions) {
-            int index = pos[0]*n + pos[1];
-            root[index] = index;
+            int x = pos[0], y = pos[1];
+            int k = x * n + y;
+            root[k] = k;
             cnt++;
 
             for (int[] d : dirs) {
-                int nx = pos[0]+d[0];
-                int ny = pos[1]+d[1];
-                int nIdx = nx*n + ny;
-                if (nx >= 0 && ny >= 0 && nx < m && ny < n && root[nIdx] != -1) {
-                    int nRoot = getRoot(nIdx, root);
-                    if (root[index] != nRoot) {
-                        root[index] = nRoot;
-                        index = nRoot;
+                int nx = x+d[0], ny = y+d[1];
+                if (nx >= 0 && nx < m && ny >= 0 && ny < n && root[nx * n + ny] != -1) {
+                    int r = getRoot(root, nx * n + ny);
+                    if (r != root[k]) {
+                        root[k] = r;
+                        k = r;
                         cnt--;
                     }
                 }
             }
-
-            res.add(cnt);
+            
+            ret.add(cnt);
         }
 
-        return res;
+        return ret;
     }
 
-    private int getRoot(int x, int[] root) {
-        while (root[x] != x) {
-            root[x] = root[root[x]];
-            x = root[x];
+    private int getRoot(int[] root, int k) {
+        while (root[k] != k) {
+            root[k] = root[root[k]];
+            k = root[k];
         }
 
-        return x;
+        return k;
     }
 }
+
